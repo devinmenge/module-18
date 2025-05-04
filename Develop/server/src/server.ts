6fb6794f0972e779aedd72bd345742ec266d8ed9
Devin Menge <devinmenge@gmail.com>
2025-05-04 13:33:38 -0400

    fixed server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,

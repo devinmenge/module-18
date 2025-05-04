d37f85f3d8a895e8278f3d324842ae990c2da11b
Devin Menge <devinmenge@gmail.com>
2025-05-04 13:47:48 -0400

    updated server.ts for root page fallback

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -36,6 +36,11 @@ const startApolloServer = async () => {
   // If we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    // Add a catch-all route to serve index.html for all unmatched routes
+    app.get('*', (_req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   }
 
   db.once('open', () => {
